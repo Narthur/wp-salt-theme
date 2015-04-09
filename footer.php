622aee285c037df516c1eac34257c9b97aaadc9e
Nathan <narthur.a@gmail.com>
2015-04-09 17:21:33 -0500

    Worked on page margins, headers, footers

--- footer.php
+++ footer.php
@@ -1,9 +1,11 @@
 <div class="clear"></div>
 </div>
 <footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
-</div>
+    <nav class="footer-nav"><?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?></nav>
+    <span class="contact">
+        <?php echo sprintf( '%1$s %2$s %3$s. All Rights Reserved.', '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?> •
+        SALT Ministries, Wat Preah Yesu • P.O. Box 93007, Siem Reap, Cambodia • Tel. (855) 12 804017, (855) 12 734102 • E-mail: <a href="mailto:salt@camshin.com.kh">salt@camshin.com.kh</a>
+    </span>
 </footer>
 </div>
 <?php wp_footer(); ?>
