0b2ea1f3a81bd3dc94d848eba82011527425438b
Nathan <narthur.a@gmail.com>
2015-04-10 10:07:52 -0500

    worked on header, added hero image

--- header.php
+++ header.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
-<title><?php wp_title( ' | ', true, 'right' ); ?></title>
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
-<?php wp_head(); ?>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width" />
+    <title><?php wp_title( ' | ', true, 'right' ); ?></title>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
+    <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-    <h2 id="site-title">
-        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a>
-    </h2>
-    <div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
-</header>
-<div id="container">
+    <div id="wrapper" class="hfeed">
+        <header id="header" role="banner">
+            <a id="branding" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home">
+                <h2 id="site-title">
+                    <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+                </h2>
+                <div id="site-description"><?php bloginfo( 'description' ); ?></div>
+            </a>
+            <img src="<?php echo get_template_directory_uri(); ?>/images/kids.png" alt="Children smiling" class="hero-img" />
+            <nav id="menu" role="navigation">
+                <div id="search">
+                <?php get_search_form(); ?>
+                </div>
+                <?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+            </nav>
+        </header>
+        <div id="container">
