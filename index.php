7aa35f5e26f78eef4ffd8ac0d8c16a8865bc8032
Nathan Arthur <narthur.a@gmail.com>
2015-12-04 12:23:28 -0600

    Layed out index content area

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php get_header(); ?>
 <section id="content" role="main">
+    <h3>News</h3>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part( 'entry' ); ?>
 <?php comments_template(); ?>
